5b028517c7b12fd1013ad72a94f5dd41e2f3c982
新轻量服务器 <1203970284@qq.com>
2023-08-23 23:29:36 +0800

    feat: add

--- website/src/components/Route.tsx
+++ website/src/components/Route.tsx
@@ -32,7 +32,7 @@ export default function Route({
   puppeteer?: boolean;
   children?: JSX.Element | JSX.Element[];
 }): JSX.Element {
-    const demoUrl = 'https://rsshub.app' + example;
+    const demoUrl = 'https://rss.xhhzs.cn' + example;
     const paramMatch = path.match(/(?<=:).*?(?=\/|$)/g);
 
     const renderMarkdown = (item, inline = true) => {
